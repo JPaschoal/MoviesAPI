daea7b5f08c778d4eb2d46dccfec6c2c9fc918d5
Jhonatan Paschoal <jhonatan.paschoal@performait.com>
2024-04-15 23:59:07 -0300

    Add controller support to MoviesAPI project

---
+++ MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> _movies = new List<Movie>();
+
+    [HttpPost]
+    public void AddMovie([FromBody] Movie movie)
+    {
+        _movies.Add(movie);
+    }
+}
