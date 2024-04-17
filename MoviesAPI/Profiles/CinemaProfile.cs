77f59e9376b3f9cdeb7c55c135fda676b6b30b86
Jhonatan Paschoal <jhonatan.paschoal@performait.com>
2024-04-17 20:21:28 -0300

    Update ReadCinemaDto property name to Address

--- MoviesAPI/Profiles/CinemaProfile.cs
+++ MoviesAPI/Profiles/CinemaProfile.cs
@@ -11,6 +11,6 @@ public class CinemaProfile : Profile
         CreateMap<CreateCinemaDto, Cinema>();
         CreateMap<UpdateCinemaDto, Cinema>();
         CreateMap<Cinema, UpdateCinemaDto>();
-        CreateMap<Cinema, ReadCinemaDto>().ForMember(cinemaDto => cinemaDto.ReadAddressDto, opt => opt.MapFrom(c => c.Address));
+        CreateMap<Cinema, ReadCinemaDto>().ForMember(cinemaDto => cinemaDto.Address, opt => opt.MapFrom(c => c.Address));
     }
 }
